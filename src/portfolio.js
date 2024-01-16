bc3e733f9dab4b91f6d6bb3dc3a8ad0beb1b1806
SystemNoe <155932802+noediscord34@users.noreply.github.com>
2024-01-16 11:16:11 -0500

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,7 +20,7 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
+  username: "Noe Saad",
   title: "Hi all, I'm Saad",
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
@@ -33,9 +33,9 @@ const greeting = {
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
+  github: "https://github.com/noediscord34",
+  linkedin: "https://www.linkedin.com/in/saadnoe/",
+  gmail: "noe@cactus.com",
   gitlab: "https://gitlab.com/saadpasta",
   facebook: "https://www.facebook.com/saad.pasta7",
   medium: "https://medium.com/@saadpasta",
